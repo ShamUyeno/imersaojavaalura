b001444769c5e4a7d6cfe803bfdeb27141af18fa
Sham <92831285+ShamUyeno@users.noreply.github.com>
2023-03-30 10:07:19 -0300

    aula 2

---
+++ Projetojavaalura/src/app2.java
@@ -0,0 +1,66 @@
+﻿import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+
+public class app2 {
+    public static void main(String[] args) throws Exception {
+        
+        // conexao http e buscar os filmes mais populares
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient(); 
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+        
+        // extrair somente os dados que interessa (titulo, poster, pontuaçao)
+        var parse = new jsonParse();
+        List<Map<String, String>> listaDeFilmes = parse.parse(body);
+
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();       
+
+        //exibir e manuiular os dados
+         
+        for (Map<String, String> filme : listaDeFilmes){
+            String urlImagem = filme.get("image");
+            String urlImagemMaior = urlImagem.replaceFirst("(@?\\.)([0-9A-Z,_]+).jpg$", "");
+            String titulo = filme.get("title");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+
+            String textofigurinha;
+            InputStream imagemEmoji;
+            if (classificacao >= 9.0) {
+                textofigurinha = "EU INDICO!";
+                imagemEmoji = new FileInputStream(new File("sobreposicao/otimo.png"));
+            }else if (classificacao >= 8.0) {
+                textofigurinha = "ASSISTA JA!";
+                imagemEmoji = new FileInputStream(new File("sobreposicao/bom.png"));
+            } else {
+                textofigurinha = "ERRRRR!";
+                imagemEmoji = new FileInputStream(new File("sobreposicao/ruim.png"));
+            }
+
+            InputStream inputStream = new URL(urlImagemMaior).openStream();
+            String nomeFilme = "figurinhas/"+ titulo + ".png";
+            String nomeArquivo = nomeFilme.replace(":", "-");
+
+            var geradora = new geradoradefigurinha();
+            geradora.cria(inputStream, nomeArquivo, textofigurinha, imagemEmoji); 
+
+            System.out.println(filme.get("title"));
+            System.out.println();
+        }
+
+    }
+}
---
+++ Projetojavaalura/src/geradoradefigurinha.java
@@ -0,0 +1,82 @@
+﻿import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+//import java.io.FileInputStream;
+import java.io.InputStream;
+
+import javax.imageio.ImageIO;
+
+
+
+public class geradoradefigurinha {
+    
+
+    public void cria(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStreamSobreposicao) throws Exception{
+
+    //leitura da imagem
+    //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+    BufferedImage imagemOriginal = ImageIO.read(inputStream); 
+
+    //cria uma nova imagem (transparencia e tamanho novo)
+    int largura = imagemOriginal.getWidth();
+    int altura = imagemOriginal.getHeight();
+    int novaaltura = altura + 200;
+    BufferedImage novaImagem = new BufferedImage(largura, novaaltura, BufferedImage.TRANSLUCENT);
+
+    //copiar imagem para novo imagem (memoria)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0,0, null);
+
+        BufferedImage imagemSobreposicao = ImageIO.read(inputStreamSobreposicao);
+        int posicaoImagemSobreposicaoY = novaaltura - imagemSobreposicao.getHeight();
+        graphics.drawImage(imagemSobreposicao, 0, posicaoImagemSobreposicaoY, null);
+
+    // Configurar a fonte
+    Font fonte = new Font("Impact", Font.BOLD, 120 );
+    graphics.setColor(Color.green);
+    graphics.setFont(fonte);
+
+    // Escrever a frase na nova imagem centralizada
+    FontMetrics FontMetrics = graphics.getFontMetrics();
+    Rectangle2D retangulo = FontMetrics.getStringBounds(texto, graphics);
+    int larguratexto = (int) retangulo.getWidth();
+    int posicaotextoX = (largura - larguratexto)/2;
+    int posicaotextoY = novaaltura - 50;
+    graphics.drawString(texto, posicaotextoX, posicaotextoY);
+
+   FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+   var textLayout = new TextLayout(texto, fonte, fontRenderContext);
+        
+   Shape outline = textLayout.getOutline(null); 
+   AffineTransform transform = graphics.getTransform();
+   transform.translate(posicaotextoX, posicaotextoY);
+   graphics.setTransform(transform);
+
+   var outlineStroke = new BasicStroke(largura * 0.004f);
+   graphics.setStroke(outlineStroke);
+
+   graphics.setColor(Color.RED);
+   graphics.draw(outline);
+   graphics.setClip(outline);     
+
+    // escrever nova imagem 
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+    }
+
+//    public static void main(String[] args) throws Exception {
+//        var geradora = new geradoradefigurinha();
+//        geradora.cria();
+//    }
+
+}
